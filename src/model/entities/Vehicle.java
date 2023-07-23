7d9c92a622e415230256896cee2f6c8516bb96e5
do5-5anto5 <tarraske.delico@gmail.com>
2023-07-22 22:15:31 -0300

    practcing interface

---
+++ src/model/entities/Vehicle.java
@@ -0,0 +1,19 @@
+package model.entities;
+
+public class Vehicle {
+
+	private String vehicle;
+
+	public Vehicle(String vehicle) {
+		this.vehicle = vehicle;
+	}
+
+	public String getVehicle() {
+		return vehicle;
+	}
+
+	public void setVehicle(String vehicle) {
+		this.vehicle = vehicle;
+	}	
+	
+}
